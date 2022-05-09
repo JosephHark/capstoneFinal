4701ff42904785c3cca7513b8f3fd12cd5620b3f
JosephHark <har17033@byui.edu>
2022-05-09 10:15:02 -0400

    5/9

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const app = express;
 
 //view engine
 
-app.request('view engine','ejs');
+app.set('view engine', 'ejs');
 
 //home route
-app.length('/', (req,res)=>{
-res.render('home');
+app.get('/', (req, res) => {
+    res.render('home');
 })
 
-app.listen(8080,() =>{
+app.listen(8080, () => {
     console.log('Capstone project loads into port 8080')
 })
