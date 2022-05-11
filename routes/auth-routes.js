75dcaa3fbf63b92bb84adf246a1a97bdb0f8714f
JosephHark <har17033@byui.edu>
2022-05-11 16:02:08 -0400

    logout

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -7,14 +7,14 @@ router.get('/login', (req, res) => {
 
 //logout
 router.get('/logout', (req, res) => {
-    res.send('logout with google');
+    req.logout();
+    res.redirect('/');
 });
 
 //router with google
 router.get('/google', passport.authenticate('google', {
     scope: ['profile']
 }));
-
 
 //callback route for google
 router.get('/google/redirect', passport.authenticate('google'), (req,res)=>{
--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -10,6 +10,6 @@ const authCheck = (req, res, next) => {
 };
 
 router.get('/', authCheck, (req, res) => {
-    res.render('profile');
+    res.render('profile', {user:req.user});
 });
 module.exports = router;
